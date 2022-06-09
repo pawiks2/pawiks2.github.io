7110446815e837275fe7f789622a6c6736b17911
pawiks2 <107156812+pawiks2@users.noreply.github.com>
2022-06-09 10:33:24 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDzn5E_vtRwtIBWuFoR50uV0l7KqS3ZOBs",
+  authDomain: "inventory-system-b5aed.firebaseapp.com",
+  projectId: "inventory-system-b5aed",
+  storageBucket: "inventory-system-b5aed.appspot.com",
+  messagingSenderId: "276474222792",
+  appId: "1:276474222792:web:0e48eeff78fb7000ebadfd",
+  measurementId: "G-WXZT830CVT"
+};
+  firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
